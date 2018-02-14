802bd82792d73f79cb6c315c0983be4d3caada70
Laurent Kleering vb <laurent.kleering.van.beerenbergh@hva.nl>
2018-02-15 00:01:32 +0100

    - Added missing routes callbacks

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Card;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
